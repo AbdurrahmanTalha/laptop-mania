749d6f829e1a59a8b679128a823d20088b919ce4
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-04-04 02:31:40 +0600

    finished just need to add the question and do some designing for the 404 page

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -24,17 +24,7 @@ const Dashbord = () => {
                 </div>
                 <div className="col-md-6">
                     <h2>Investment VS Revenue</h2>
-                    <BarChart
-                        width={500}
-                        height={300}
-                        data={data}
-                        margin={{
-                            top: 20,
-                            right: 30,
-                            left: 20,
-                            bottom: 5
-                        }}
-                    >
+                    <BarChart width={500}height={300}data={data}margin={{top: 20,right: 30,left: 20,bottom: 5}}>
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
                         <YAxis />
--- src/Components/ReviewsCard/ReviewsCard.js
+++ src/Components/ReviewsCard/ReviewsCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./ReviewsCard.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 const ReviewsCard = (props) => {
-    const { picture, name, comment } = props.review;
+    const { picture, name, comment, rating } = props.review;
     return (
         <div className="col-md-4">
             <img src={picture} alt="" className="review-img rounded-circle"></img>
             <div className="review-details">
                 <h2>{name}</h2>
                 <p className="review-desc" title={comment}>{comment.slice(0, 50)}..</p>
-                <p></p>
+                <p>Rating: <FontAwesomeIcon icon={faStar} className="rating-icon"></FontAwesomeIcon> {rating}</p>
             </div>
         </div>
     );
