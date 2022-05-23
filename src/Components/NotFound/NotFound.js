d7173ff72358dd6d023054c4b3f31e2335d6518b
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-05-23 18:31:58 +0600

    changed github origin

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className="my-5">
-            <h1>The page you are looking for is not found 404</h1>
+            <h1>Page not found</h1>
             <img src="https://webdeasy.de/wp-content/uploads/2020/06/404-pages.jpg" alt=""></img>
         </div>
     );
